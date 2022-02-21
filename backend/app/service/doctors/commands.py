a4edb4965136cc4e5c9a45df55aca5cffca2d8fe
annatrz <trzcinska.ania@gmail.com>
2022-02-21 09:28:29 +0100

    Return 204 from delete endpoint

--- backend/app/service/doctors/commands.py
+++ backend/app/service/doctors/commands.py
@@ -5,26 +5,33 @@ from service.doctors.models import Doctor
 from fastapi import APIRouter, Body, Depends, Path
 from pydantic import BaseModel
 from sqlalchemy.orm.session import Session
+from starlette import status
+from starlette.responses import Response
 
 router = APIRouter()
+
 
 class DoctorCreate(BaseDoctor):
     pass
 
+
 @router.post("/doctors", status_code=201)
 def create_doctor(
-    doctor: DoctorCreate = Body(default=...),
-    db: Session = Depends(db_session)
+        doctor: DoctorCreate = Body(default=...),
+        db: Session = Depends(db_session)
 ) -> Doctor:
     new_doctor = DoctorOrm(**doctor.dict())
     db.add(new_doctor)
     db.commit()
     return Doctor.from_orm(new_doctor)
 
-@router.delete("/doctors/{doctor_id}", status_code=200)
-def create_doctor(
-    doctor_id: int = Path(default=...),
-    db: Session = Depends(db_session)
-) -> Doctor:
+
+@router.delete("/doctors/{doctor_id}", status_code=204)
+def delete_doctor(
+        doctor_id: int = Path(default=...),
+        db: Session = Depends(db_session)
+) -> Response:
     db.query(DoctorOrm).filter(DoctorOrm.id == doctor_id).delete()
     db.commit()
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
