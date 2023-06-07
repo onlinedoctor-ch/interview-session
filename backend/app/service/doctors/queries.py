afda48399a939b987c73f5722a1ac6841c2562a8
Jonas Berdoz <jonas.berdoz@gmail.com>
2023-06-07 17:53:58 +0200

    Fix type

--- backend/app/service/doctors/queries.py
+++ backend/app/service/doctors/queries.py
@@ -18,7 +18,7 @@ class DoctorCreate(BaseDoctor):
 
 
 @router.get("/doctors")
-def get_doctors(db: Session = Depends(db_session)) -> Doctor:
+def get_doctors(db: Session = Depends(db_session)) -> list[Doctor]:
     doctors = [
         Doctor.from_orm(db_doctor)
         for db_doctor in db.query(DoctorOrm).order_by(DoctorOrm.id.desc()).all()
